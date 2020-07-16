37fcffd7cb429f979706c47cbbc7146717f0af48
aLIEzsss4 <vuetomato@gmail.com>
2020-07-16 11:30:10 +0800

    fix: 修复3.0版本错误

--- src/EChart/index.js
+++ src/EChart/index.js
@@ -15,34 +15,25 @@ export default class EChart extends Component {
     };
   }
 
+  componentDidMount() {
+    const { lazyLoad } = this.props;
+    if (!lazyLoad) {
+      Taro.nextTick(() => {
+        this.init();
+      });
+    }
+  }
+
   componentDidUpdate(prevProps) {
     if (this.props.option !== prevProps.option) {
       this.setOption(this.props.option);
     }
   }
 
-  componentDidMount() {
-    const { lazyLoad } = this.props;
-    if (!lazyLoad) {
-      this.init();
-    }
-  }
-
-  render() {
-    const { disableTouch, style } = this.props;
-    const canvasId = this.getCanvasId();
-    return (
-      <Canvas
-        type="2d"
-        className="techarts-canvas"
-        id={canvasId}
-        canvasId={canvasId}
-        style={style}
-        onTouchStart={disableTouch ? '' : this._touchStart}
-        onTouchMove={disableTouch ? '' : this._touchMove}
-        onTouchEnd={disableTouch ? '' : this._touchEnd}
-      ></Canvas>
-    );
+  TaroCreateSelectorQuery() {
+    return Taro.useReady
+      ? Taro.createSelectorQuery()
+      : Taro.createSelectorQuery().in(this.$scope)
   }
 
   setOption(option) {
@@ -59,7 +50,7 @@ export default class EChart extends Component {
     if (!this.echarts) {
       console.error(
         '[EChart]：组件需要echarts对象才能绘图，建议去官网自定义构建。' +
-          '注意不要勾选“代码压缩”，可下载后自行压缩。https://www.echartsjs.com/zh/builder.html'
+        '注意不要勾选“代码压缩”，可下载后自行压缩。https://www.echartsjs.com/zh/builder.html'
       );
       return;
     }
@@ -94,8 +85,8 @@ export default class EChart extends Component {
         if (!isValid) {
           console.error(
             '微信基础库版本过低，需大于等于 1.9.91。' +
-              '参见：https://github.com/ecomfe/echarts-for-weixin' +
-              '#%E5%BE%AE%E4%BF%A1%E7%89%88%E6%9C%AC%E8%A6%81%E6%B1%82'
+            '参见：https://github.com/ecomfe/echarts-for-weixin' +
+            '#%E5%BE%AE%E4%BF%A1%E7%89%88%E6%9C%AC%E8%A6%81%E6%B1%82'
           );
         } else {
           console.warn('建议将微信基础库调整大于等于2.9.0版本。升级后绘图将有更好性能');
@@ -126,7 +117,7 @@ export default class EChart extends Component {
       });
       // const canvasDpr = Taro.getSystemInfoSync().pixelRatio // 微信旧的canvas不能传入dpr
       const canvasDpr = 1;
-      var query = Taro.createSelectorQuery().in(this.$scope);
+      var query = this.TaroCreateSelectorQuery();
       query
         .select('.techarts-canvas')
         .boundingClientRect(res => {
@@ -140,7 +131,7 @@ export default class EChart extends Component {
     if (process.env.TARO_ENV !== 'h5') {
       // version >= 2.9.0：使用新的方式初始化
       const canvasId = this.getCanvasId();
-      const query = Taro.createSelectorQuery().in(this.$scope);
+      const query = this.TaroCreateSelectorQuery();
       query
         .select('.techarts-canvas')
         .fields({ node: true, size: true })
@@ -174,7 +165,7 @@ export default class EChart extends Component {
     } else {
       if (this.state.isUseNewCanvas) {
         // 新版
-        const query = Taro.createSelectorQuery().in(this.$scope);
+        const query = this.TaroCreateSelectorQuery();
         query
           .select('.techarts-canvas')
           .fields({ node: true, size: true })
@@ -239,7 +230,7 @@ export default class EChart extends Component {
   _initChart(canvas, width, height, dpr) {
     const { onInit } = this.props;
     if (!canvas.setChart) {
-      canvas.setChart = () => {};
+      canvas.setChart = () => { };
     }
     if (typeof onInit === 'function') {
       this.chart = onInit(canvas, width, height, dpr);
@@ -256,6 +247,23 @@ export default class EChart extends Component {
       this.chart = chart;
     }
     return this.chart;
+  }
+
+  render() {
+    const { disableTouch, style } = this.props;
+    const canvasId = this.getCanvasId();
+    return (
+      <Canvas
+        type="2d"
+        className="techarts-canvas"
+        id={canvasId}
+        canvasId={canvasId}
+        style={style}
+        onTouchStart={disableTouch ? '' : this._touchStart}
+        onTouchMove={disableTouch ? '' : this._touchMove}
+        onTouchEnd={disableTouch ? '' : this._touchEnd}
+      ></Canvas>
+    );
   }
 }
 
